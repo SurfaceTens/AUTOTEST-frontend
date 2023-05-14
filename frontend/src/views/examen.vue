71025365ef379a546b374443f6e0f9bd585573ef
SurfaceTens <sergio_perea_91@hotmail.com>
2023-05-14 03:36:17 +0200

    El examen se hace y se corrije.

--- frontend/src/views/examen.vue
+++ frontend/src/views/examen.vue
@@ -10,6 +10,9 @@ export default {
     data() {
         return {
             numPreguntas: 30, // Numero de preguntas que debe tener el examen.
+            examenTerminado: false, // Variable para controlar el estado del examen.
+            respuestasExamen: [], // Array para almacenar las respuestas del examen.
+            tituloExamen: `Examen aleatorio de 30 preguntas`, // Título inicial del examen.
         };
     },
     computed: {
@@ -18,7 +21,7 @@ export default {
             return this.randomizarYLimitarPreguntas(this.preguntas, this.numPreguntas);
         },
         pregunta() {
-            return this.preguntas.find(p => p.id == this.$route.params.id);
+            return this.preguntas.find((p) => p.id === this.$route.params.id);
         },
     },
     methods: {
@@ -28,48 +31,165 @@ export default {
             const totalPreguntas = preguntas.length;
             const cantidadFactible = Math.min(totalPreguntas, cantidad);
             const preguntasDesorden = this.desordenarElArray(preguntas);
+
+            // Reiniciar respuestas de preguntas anteriores
+            preguntasDesorden.forEach((pregunta) => {
+                pregunta.respuesta = null;
+            });
+
             return preguntasDesorden.slice(0, cantidadFactible);
         },
 
         desordenarElArray(array) {
-            // Algoritmo de Fisher-Yates para desordenar el array    
-            // Obtener la longitud del array y crear un valor temporal.
             let indiceActual = array.length;
             let valorTemporal, numAleatorio;
 
-            // Mientras no se hayan recorrido todos los elementos del array:
             while (indiceActual !== 0) {
-                // Generar un índice aleatorio entre 0 y el actual.
                 numAleatorio = Math.floor(Math.random() * indiceActual);
                 indiceActual--;
 
-                // Intercambiar el contenido de ambos indices.
                 valorTemporal = array[indiceActual];
                 array[indiceActual] = array[numAleatorio];
                 array[numAleatorio] = valorTemporal;
             }
             return array;
+        },
+
+        seleccionarOpcion(pregunta, opcion) {
+            pregunta.respuesta = opcion;
+        },
+
+        guardarRespuestas() {
+            this.respuestasExamen = this.preguntasTratadas.map((pregunta) => {
+                return {
+                    id: pregunta.id,
+                    respuesta: pregunta.respuesta,
+                };
+            });
+        },
+
+        terminarExamen() {
+            if (confirm('¿Estás seguro de que deseas terminar el examen?')) {
+                this.guardarRespuestas();
+                this.mostrarRespuestas();
+                this.calcularPuntuacion();
+                this.examenTerminado = true;
+                window.scrollTo({ top: 0, behavior: 'smooth' });
+            }
+        },
+
+        mostrarRespuestas() {
+            this.preguntasTratadas.forEach((pregunta) => {
+                pregunta.respuestaCorrecta = pregunta.respuesta === pregunta.correcta;
+                pregunta.respuestaIncorrecta = pregunta.respuesta && pregunta.respuesta !== pregunta.correcta;
+            });
+        },
+
+        getRespuestaSeleccionada(preguntaId) {
+            const respuesta = this.respuestasExamen.find(respuesta => respuesta.id === preguntaId);
+            return respuesta ? respuesta.respuesta : '';
+        },
+
+        calcularPuntuacion() {
+            let acertadas = 0;
+            this.respuestasExamen.forEach((respuesta) => {
+                const pregunta = this.preguntasTratadas.find((pregunta) => pregunta.id === respuesta.id);
+                if (pregunta) {
+                    if (respuesta.respuesta === pregunta.correcta) {
+                        acertadas++;
+                    }
+                }
+            });
+
+            const totalPreguntas = this.preguntasTratadas.length;
+            const porcentajeAciertos = (acertadas / totalPreguntas) * 100;
+            let resultado;
+
+            if (porcentajeAciertos >= 90) {
+                resultado = "APTO";
+            } else {
+                resultado = "NO APTO";
+            }
+
+            this.tituloExamen = `Aciertos: ${acertadas} Resultado: ${resultado}`;
+        },
+        generarNuevoExamen() {
+            this.preguntasTratadas = this.randomizarYLimitarPreguntas(this.preguntas, this.numPreguntas);
+            this.respuestasExamen = [];
+            this.tituloExamen = `Examen aleatorio de ${this.numPreguntas} preguntas`;
+            this.examenTerminado = false;
+            window.scrollTo({ top: 0, behavior: 'smooth' });
         }
+
     },
     created() {
         this.getPreguntas();
-    }
+    },
 };
 </script>
 
 <template>
-    <div>
-        <h1>Examen aleatorio de {{ numPreguntas }} preguntas</h1>
-        <ul>
-            <li v-for="(pregunta) in preguntasTratadas" :key="id">
-                <pregunta :pregunta="pregunta" />
-            </li>
-        </ul>
+    <div class="area-examen">
+        <!-- Versión resoluble del examen -->
+        <div v-if="!examenTerminado">
+            <h1>{{ tituloExamen }}</h1>
+            <ul>
+                <li v-for="(pregunta) in preguntasTratadas" :key="pregunta.id">
+                    <pregunta :pregunta="pregunta" @opcionSeleccionada="seleccionarOpcion(pregunta, $event)"
+                        :respuestaCorrecta="pregunta.respuestaCorrecta"
+                        :respuestaIncorrecta="pregunta.respuestaIncorrecta" />
+                    <b-button-group size="sm" vertical>
+                        <b-button v-for="opcion in pregunta.opciones" :key="opcion.id"
+                            @click="seleccionarOpcion(pregunta, opcion.id)" :class="{
+                                'btn-primary': pregunta.respuesta === opcion.id,
+                                'btn-outline-primary': pregunta.respuesta !== opcion.id,
+                            }">
+                            {{ opcion.texto }}
+                        </b-button>
+                    </b-button-group>
+                </li>
+            </ul>
+            <div class="fin-examen">
+                <button @click="terminarExamen" class="btn btn-primary btn-lg">Terminar</button>
+            </div>
+        </div>
+
+        <!-- Versión del examen con respuestas resaltadas -->
+        <div v-else>
+            <h1>{{ tituloExamen }}</h1>
+            <ul>
+                <li v-for="(pregunta) in preguntasTratadas" :key="pregunta.id">
+                    <pregunta :pregunta="pregunta" :respuestaCorrecta="pregunta.correcta"
+                        :respuestaSeleccionada="getRespuestaSeleccionada(pregunta.id)" />
+                </li>
+
+            </ul>
+            <div class="fin-examen">
+                <button @click="generarNuevoExamen" class="btn btn-secondary btn-lg">Hacer Otro</button>
+            </div>
+
+        </div>
     </div>
 </template>
 
 <style scoped>
+.area-examen {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 20px;
+    margin-bottom: 20px;
+}
+
 ul {
     list-style: none;
 }
+
+.fin-examen {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 20px;
+}
 </style>
+  
--- frontend/src/views/pregunta.vue
+++ frontend/src/views/pregunta.vue
@@ -1,111 +1,156 @@
 <script>
 export default {
-    props: {
-        pregunta: {
-            type: Object,
-            required: true
-        }
+  props: {
+    pregunta: {
+      type: Object,
+      required: true,
     },
-    computed: {
-        // Hacer un array con las alternativas.
-        alternativas() {
-            if (this.pregunta) {
-                const correcta = this.pregunta.correcta;
-                const incorrectas = this.pregunta.incorrectas;
-                return [correcta, ...incorrectas];
-            }
-            return [];
-        },
-        // Crear un array desordenado.
-        alternativasAleatorias() {
-            return this.desordenarArray(this.alternativas);
-        }
+    respuestaCorrecta: {
+      type: String,
+      required: true,
     },
-    methods: {
-        desordenarArray(array) {
-            // Algoritmo de Fisher-Yates para desordenar el array    
-            // Obtener la longitud del array y crear un valor temporal.
-            let indiceActual = array.length;
-            let valorTemporal, numAleatorio;
-
-            // Mientras no se hayan recorrido todos los elementos del array:
-            while (indiceActual !== 0) {
-                // Generar un índice aleatorio entre 0 y el actual.
-                numAleatorio = Math.floor(Math.random() * indiceActual);
-                indiceActual--;
-
-                // Intercambiar el contenido de ambos indices.
-                valorTemporal = array[indiceActual];
-                array[indiceActual] = array[numAleatorio];
-                array[numAleatorio] = valorTemporal;
-            }
-            return array;
-        }
+    respuestaSeleccionada: {
+      type: String,
+      default: '',
     }
+  },
+  computed: {
+    // Hacer un array con las alternativas.
+    alternativas() {
+      if (this.pregunta) {
+        const correcta = this.pregunta.correcta;
+        const incorrectas = this.pregunta.incorrectas;
+        return [correcta, ...incorrectas];
+      }
+      return [];
+    },
+    // Crear un array desordenado.
+    alternativasAleatorias() {
+      return this.desordenarArray(this.alternativas);
+    },
+
+    alternativaSeleccionada() {
+      return this.pregunta.respuesta;
+    },
+    mostrarRespuestas() {
+      return this.respuestaCorrecta !== '' && this.respuestaSeleccionada !== '';
+    },
+    esRespuestaCorrecta() {
+      return this.respuestaCorrecta === this.respuestaSeleccionada;
+    }
+  },
+
+  methods: {
+    desordenarArray(array) {
+      // Algoritmo de Fisher-Yates para desordenar el array    
+      // Obtener la longitud del array y crear un valor temporal.
+      let indiceActual = array.length;
+      let valorTemporal, numAleatorio;
+
+      // Mientras no se hayan recorrido todos los elementos del array:
+      while (indiceActual !== 0) {
+        // Generar un índice aleatorio entre 0 y el actual.
+        numAleatorio = Math.floor(Math.random() * indiceActual);
+        indiceActual--;
+
+        // Intercambiar el contenido de ambos indices.
+        valorTemporal = array[indiceActual];
+        array[indiceActual] = array[numAleatorio];
+        array[numAleatorio] = valorTemporal;
+      }
+      return array;
+    },
+
+    asignarClaseRespuesta(opcion) {
+      if (this.mostrarRespuestas) {
+        if (opcion === this.respuestaCorrecta && this.esRespuestaCorrecta) {
+          return 'respuesta-correcta';
+        } else if (opcion === this.respuestaSeleccionada && !this.esRespuestaCorrecta) {
+          return 'respuesta-incorrecta';
+        }
+      }
+      return '';
+    }
+  },
 };
 </script>
 
 <template>
-    <div class="card pregunta-card">
-      <div class="card_header">
-        <h3 class="card_title">{{ pregunta.id }} - {{ pregunta.enunciado }}</h3>
-        <div>
-          <!-- Mostrar la imagen de la pregunta si está disponible -->
-          <img v-if="pregunta.imagen" :src="pregunta.imagen" alt="Imagen de la pregunta" />
-          <p v-else>No hay imagen disponible</p>
-        </div>
+  <div class="card pregunta-card">
+    <div class="card_header">
+      <h3 class="card_title">{{ pregunta.id }} - {{ pregunta.enunciado }}</h3>
+      <div>
+        <!-- Mostrar la imagen de la pregunta si está disponible -->
+        <img v-if="pregunta.imagen" :src="'@/assets/imagenesPreguntas/' + pregunta.imagen" alt="Imagen de la pregunta" />
+        <p v-else>No hay imagen disponible</p>
       </div>
-      <ul class="alternativas-list">
-        <li v-for="alternativa in alternativasAleatorias" :key="alternativa" class="alternativa-item">
-          {{ alternativa }}
-        </li>
-      </ul>
     </div>
-  </template>
+    <ul class="alternativas-list">
+      <li v-for="alternativa in alternativasAleatorias" :key="alternativa" class="alternativa-item" :class="{
+        'alternativa-seleccionada': alternativa === alternativaSeleccionada,
+        'respuesta-correcta': mostrarRespuestas && alternativa === respuestaCorrecta && esRespuestaCorrecta,
+        'respuesta-incorrecta': mostrarRespuestas && alternativa === respuestaSeleccionada && !esRespuestaCorrecta && alternativa !== respuestaCorrecta
+      }" @click="$emit('opcionSeleccionada', alternativa)">
+        {{ alternativa }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+
+
   
-  <style scoped>
-  .pregunta-card {
-    padding: 2rem;
-    border-radius: var(--border-radius);
-    box-shadow: 0 0 8px 2px rgba(0, 0, 0, 0.1);
-    background-color: var(--color-fondo);
-    margin: 0 1rem;
-    margin-bottom: 1.5rem;
-  }
-  
-  .card_header {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin-bottom: 1.5rem;
-  }
-  
-  .card_title {
-    font-size: 1.5rem;
-    font-weight: 600;
-    color: var(--color-borde);
-  }
-  
-  .alternativas-list {
-    display: grid;
-    grid-template-columns: repeat(2, 1fr);
-    gap: 1rem;
-    padding: 0;
-    margin: 0;
-    list-style-type: none;
-  }
-  
-  .alternativa-item {
-    background-color: var(--color-secundario);
-    padding: 1rem;
-    border-radius: 8px;
-    color: var(--color-borde);
-  }
-  
-  @media (max-width: 767px) {
-    .pregunta-card {
-      padding: 1rem;
-    }
-  }
-  </style>
-  
+<style scoped>
+.card {
+  background-color: var(--color-fondo);
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  padding: 20px;
+  margin-bottom: 20px;
+  margin-right: 20px;
+}
+
+.card_header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 10px;
+}
+
+.card_title {
+  margin: 0;
+}
+
+.alternativas-list {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 10px;
+  list-style: none;
+  padding: 0;
+}
+
+.alternativa-item {
+  background-color: var(--color-secundario);
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  padding: 10px;
+  margin-bottom: 10px;
+  cursor: pointer;
+}
+
+.alternativa-item:hover {
+  background-color: #ebebeb;
+}
+
+.alternativa-seleccionada {
+  box-shadow: 0 0 5px rgba(0, 47, 255, 0.8);
+}
+
+.respuesta-correcta {
+  box-shadow: 0 0 5px rgba(0, 255, 0, 0.8);
+}
+
+.respuesta-incorrecta {
+  box-shadow: 0 0 5px rgba(255, 0, 0, 0.8);
+}
+</style>
